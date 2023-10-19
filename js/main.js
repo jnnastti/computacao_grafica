8cf5ed780b8d8331eacc6b35c63fadbfe58b0de8
Jannaina <jannaina.sangaletti@senior.com.br>
2023-10-18 21:41:08 -0300

    tratar dados

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+aviao = []
+
+// show hidden tela
 function habilitarDesabilitarTela(nome) {
     let tela = document.getElementById(`${nome}`)
 
@@ -11,6 +14,7 @@ function habilitarDesabilitarTela(nome) {
     return
 }
 
+// checkar aviao no data grid
 function checkAviao(id) {
     let aviao = document.getElementById(`aviao${id}`)
 
@@ -24,56 +28,81 @@ function checkAviao(id) {
     
 }
 
-function inserirAviao(){
-    let x, y, raio, angulo
-    let coorX = document.querySelector('#coorX').value
-    let coorY = document.querySelector('#coorY').value
-    let coorRaio = document.querySelector('#coorRaio').value
-    let coorAngulo = document.querySelector('#angulo').value
-    let inputVelocidade = document.querySelector('#velocidade').value // tem que criar esse campo ainda pelo o que vi
-    let inputDirecao = document.querySelector('#direcao').value
-
-    if (coorX != null && coorY != null){
-        // foi inserido as coordenadas cartesianas, por isso colocamos as polares como null
-        coorRaio   = null
-        coorAngulo = null
-
-        x = coorX
-        y = coorY
-        raio, angulo = cartesianoParaPolar(x, y)
-        // chamar a função que o david criou para transformar cartesiano em polar, não sei fazer isso do JS pro PHP :/ e depois atribuir o valor as lets angulo e raio
-
-    }
-    
-    if (coorRaio != null && coorAngulo != null){
-        // foi inserido as coordenadas polares, por isso colocamos as cartesianas como null        
-        coorX = null
-        coorY = null
-
-        angulo = coorAngulo
-        raio   = coorRaio
-        x, y = polarParaCartesiano(raio, angulo)
-        // chamar função que o david criou para transformar polar em cartesiano, não sei fazer isso do JS pro php :/ e depois atribuir o valor as lets x e y
-        
+// verifica se campos estão preenchidos
+function verificaCampos(x, y, r, a, d) {
+    if(d == null || d == '') {
+        return false
     }
 
-    // ao chegar nesse ponto do código ja deve ter todos os valores x, y, angulo, raio, velocidade e direcao preenchidos
-    if (x && y && angulo && raio && inputVelocidade && inputDirecao) {
-        // verificar se ja não existem 10 avioes no datagrid e adicionar caso tenha menos de 10 essa parte pelo o que vi está hardcoded ainda então não da pra fazer por hora
+    if(((x == null || x == '') && (y == null || y == '')) || ((r == null || r == '') && (a == null || a == ''))) {
+        return false
+    }
+
+    return true
+}
+
+// inserir avião no datagrid
+function inserirAviao() {
+    let coorX           = document.querySelector('#coorX').value
+    let coorY           = document.querySelector('#coorY').value
+    let coorRaio        = document.querySelector('#coorRaio').value
+    let coorAngulo      = document.querySelector('#angulo').value
+    let inputDirecao    = document.querySelector('#direcao').value
+
+    if(verificaCampos(coorX,coorY, coorRaio, coorAngulo, inputDirecao)) {
+        if(!((coorX == null || coorX == '') && (coorY == null || coorY == ''))) {
+            cartesianoParaPolar(x, y)
+        }
+
+        if(!((raio == null || raio == '') && (angulo == null || angulo == ''))) {
+            polarParaCartesiano(raio, angulo)
+        }
     }
 }
 
-// não vi exatamente onde pode ser usado isso mas ja deixei feito para que possa ser usado posteriormente, todos os inputs que vi são number e number por si só não permite número fracionado
-function substituirVirgulaPorPonto(input) {
-    // Obtém o valor do campo de entrada
-    let valor = input.value;
-    // Substitui todas as vírgulas por pontos
-    valor = valor.replace(/,/g, '.');
-    // Define o valor modificado de volta no campo de entrada
-    return valor;
+// função para converter cartesiano para polar
+function cartesianoParaPolar(x, y) {
+    let raio = Math.sqrt(x * x + y * y);
+    let angulo = Math.atan2(y, x);
+
+    if(aviao.length < 10) {
+        aviao.push({
+            x : x,
+            y : y,
+            raio : raio,
+            angulo : angulo
+        })
+    }
+}
+
+// tratamento de inputs
+function tratarInputNumeros(inp) {
+    let v = inp.value
+    let valor = v.replace(/,/g, '.')
+
+    valor = valor.replace(/[^0-9|.|-]/g, '')
+
+    const pointCount = valor.split('.').length - 1;
+
+    // Se houver mais de um ponto decimal, remova os extras
+    if (pointCount > 1) {
+        valor = valor.slice(0, valor.lastIndexOf('.'))
+    }
+
+    inp.selectionEnd = inp.value.length;
+
+    return limitarCasasDecimais(valor);
 }
 
 function limitarCasasDecimais(numero) {
-    return parseFloat(numero.toFixed(2)); // duas casas após a virgula
+   const parts = numero.split('.');
+  
+    if (parts.length > 1 && parts[1].length > 2) {
+        // Limit the number of decimal places
+        const truncatedDecimal = parts[1].substring(0, 2);
+        numero = parts[0] + '.' + truncatedDecimal;
+    }
+
+    return numero
 
 }
